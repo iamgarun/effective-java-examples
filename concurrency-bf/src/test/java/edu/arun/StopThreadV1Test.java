54c97e085b0c68a9f827e6440a35ac922000818b
iamgarun <2835125+iamgarun@users.noreply.github.com>
2017-12-24 18:39:18 +0530

    Concurrency: Item 30: synchronize access to shared mutable data

---
+++ concurrency-bf/src/test/java/edu/arun/StopThreadV1Test.java
@@ -0,0 +1,38 @@
+package edu.arun;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple StopThreadV1.
+ */
+public class StopThreadV1Test
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public StopThreadV1Test(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( StopThreadV1Test.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
